3aa5828f3b25b550ae3a8fe0d04ea5bd3eb0e0ae
mmarina <m.marina.oc14@mail.ru>
2024-05-22 16:50:22 +0300

    style correction

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -11,7 +11,7 @@ from stanza.models.common.doc import Document
 from stanza.pipeline.core import Pipeline
 from stanza.utils.conll import CoNLL
 
-from core_utils.article.article import (Article, ArtifactType, get_article_id_from_filepath)
+from core_utils.article.article import Article, ArtifactType, get_article_id_from_filepath
 from core_utils.article.io import from_meta, from_raw, to_cleaned, to_meta
 from core_utils.constants import ASSETS_PATH, UDPIPE_MODEL_PATH
 from core_utils.pipeline import (AbstractCoNLLUAnalyzer, CoNLLUDocument, LibraryWrapper,
